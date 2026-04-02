29900ca2c0e8fd2d3bbcfd18537c3a7d62eb10e3
laurensalexis23-star <laurensalexis23@gmail.com>
2026-04-01 21:33:00 -0400

    Create utils/.js

---
+++ utils/.js
@@ -0,0 +1,24 @@
+async function bug(message, client, texts, num) {
+    try {
+        const remoteJid = message.key?.remoteJid;
+        await client.sendMessage(remoteJid, {
+            image: { url: `database/${num}.jpg` },
+            caption: `> ${texts}`,
+            contextInfo: {
+                externalAdReply: {
+                    title: "Join Our WhatsApp Group",
+                    body: "קยгקɭє єץєร",
+                    mediaType: 1,
+                    thumbnailUrl: `https://chat.whatsapp.com/BxfwEM0Xq0hKOUwMAmfuG8?mode=gi_t`,
+                    renderLargerThumbnail: false,
+                    mediaUrl: `${num}.jpg`,
+                    sourceUrl: `${num}.jpg`
+                }
+            }
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export default bug;
