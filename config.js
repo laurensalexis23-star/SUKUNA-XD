e4b11c220ff1ea5bbff178ccac65e09d203393c8
laurensalexis23-star <laurensalexis23@gmail.com>
2026-04-02 07:01:26 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  Number: "4915510554313" // remplacez par votre numéro WhatsApp sans mettre le signe +
+  Number: "50941144151" // remplacez par votre numéro WhatsApp sans mettre le signe +
 };
