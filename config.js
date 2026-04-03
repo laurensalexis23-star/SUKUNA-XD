e2f5146a934d078e7b8c635f716fbdad9c710e3b
SUKUNA MD <laurensalexis23@gmail.com>
2026-04-03 00:00:47 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  Number: "50956649848" // remplacez par votre numéro WhatsApp sans mettre le signe +
+  Number: "50941144151" // remplacez par votre numéro WhatsApp sans mettre le signe +
 };
